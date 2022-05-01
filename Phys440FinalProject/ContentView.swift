cff9758573cf7296c49e94e213ea05fad97fff21
bpashia <bbpashia@gmail.com>
2022-05-01 15:35:03 -0500

    Initial Commit

---
+++ Phys440FinalProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Phys440FinalProject
+//
+//  Created by Broc Pashia on 5/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Phys440FinalProject/Phys440FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Phys440FinalProjectApp.swift
+//  Phys440FinalProject
+//
+//  Created by Broc Pashia on 5/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Phys440FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
